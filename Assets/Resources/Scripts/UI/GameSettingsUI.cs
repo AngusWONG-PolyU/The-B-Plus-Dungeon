8dcdbfc51b549c2ed79c89140a717f7b93ccefc8
AngusWONG-PolyU <23118174d@connect.polyu.hk>
2026-03-18 17:36:51 +0800

    feat: add BGM and player settings for volume and brightness

--- Assets/Resources/Scripts/UI/GameSettingsUI.cs
+++ Assets/Resources/Scripts/UI/GameSettingsUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 using TMPro;
 
 public class GameSettingsUI : MonoBehaviour
@@ -14,11 +15,60 @@ public class GameSettingsUI : MonoBehaviour
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private ConfirmationUI confirmationUI;
 
+    [Header("Audio & Video Settings")]
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Slider brightnessSlider;
+    [SerializeField] private Button resetVolumeButton;
+    [SerializeField] private Button resetBrightnessButton;
+    
+    [Header("Post Processing")]
+    [SerializeField] private PostProcessVolume globalVolume;
+    private ColorGrading colorGrading;
+
     private void Start()
     {
+        // Try getting ColorGrading from the bound PostProcessVolume
+        if (globalVolume != null)
+        {
+            globalVolume.profile.TryGetSettings(out colorGrading);
+        }
+
         // Force reset to default each time the game starts
         PlayerPrefs.SetInt("TaskMode", 0);
         PlayerPrefs.SetInt("TimeLimitMode", 0);
+        PlayerPrefs.SetFloat("GlobalVolume", 1.0f);
+        PlayerPrefs.SetFloat("Brightness", -0.1f);
+        PlayerPrefs.Save();
+
+        // Load Audio and Lighting settings
+        float savedVolume = PlayerPrefs.GetFloat("GlobalVolume");
+        float savedBrightness = PlayerPrefs.GetFloat("Brightness");
+        
+        ApplyVolume(savedVolume);
+        ApplyBrightness(savedBrightness);
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = savedVolume;
+            volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+        }
+
+        if (brightnessSlider != null)
+        {
+            brightnessSlider.value = savedBrightness;
+            brightnessSlider.onValueChanged.AddListener(OnBrightnessChanged);
+        }
+
+        if (resetVolumeButton != null)
+        {
+            resetVolumeButton.onClick.AddListener(ResetVolumeToDefault);
+        }
+
+        if (resetBrightnessButton != null)
+        {
+            resetBrightnessButton.onClick.AddListener(ResetBrightnessToDefault);
+        }
+
         PlayerPrefs.Save();
 
         if (taskTypeDropdown != null)
@@ -74,6 +124,61 @@ public class GameSettingsUI : MonoBehaviour
         // 0 = 30s, 1 = 60s, 2 = Unlimited
         PlayerPrefs.SetInt("TimeLimitMode", value);
         PlayerPrefs.Save();
+    }
+
+    public void OnVolumeChanged(float value)
+    {
+        ApplyVolume(value);
+        PlayerPrefs.SetFloat("GlobalVolume", value);
+        PlayerPrefs.Save();
+    }
+
+    private void ApplyVolume(float value)
+    {
+        // Controls all AudioSources in the game globally
+        AudioListener.volume = value;
+    }
+
+    public void OnBrightnessChanged(float value)
+    {
+        ApplyBrightness(value);
+        PlayerPrefs.SetFloat("Brightness", value);
+        PlayerPrefs.Save();
+    }
+    
+    private void ApplyBrightness(float value)
+    {
+        // Control brightness using Post Processing's ColorGrading.postExposure
+        if (colorGrading != null)
+        {
+            colorGrading.postExposure.value = value;
+        }
+    }
+
+    public void ResetVolumeToDefault()
+    {
+        float defaultVolume = 1.0f;
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = defaultVolume;
+        }
+        else
+        {
+            OnVolumeChanged(defaultVolume);
+        }
+    }
+
+    public void ResetBrightnessToDefault()
+    {
+        float defaultBrightness = -0.1f;
+        if (brightnessSlider != null)
+        {
+            brightnessSlider.value = defaultBrightness;
+        }
+        else
+        {
+            OnBrightnessChanged(defaultBrightness);
+        }
     }
 
     public void ReturnToLobby()
